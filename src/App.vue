1baf10c2fbdacece988ba60748c149a6d01218ac
Nguyen Thien Dzung <dungtoanhoc@yahoo.com.vn>
2016-12-05 14:58:05 +0700

    First initialization

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>Testing</h1>
+    <gallery></gallery>
+  </div>
+</template>
+
+<script>
+import Gallery from 'components/Gallery.vue'
+
+export default {
+  name: 'app',
+
+  components: {
+    Gallery,
+  },
+
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
---
+++ src/components/Gallery.vue
@@ -0,0 +1,13 @@
+<template>
+	<h1>Hello World</h1>
+</template>
+
+<script>
+	export default {
+    data() {
+      return {
+        message: 'Hello WOrld message',
+      }
+    }
+  }
+</script>
