78dfcb918116f55f29748de91b23ce2590fcf648
Nguyen Thien Dzung <dungtoanhoc@yahoo.com.vn>
2017-02-14 08:49:41 +0700

    feat(lightbox): add lazyload and fix key

--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueLazyload from 'vue-lazyload'
 
-Vue.use(VueLazyload)
+import siteloading from './siteloading.gif'
+
+// Vue.use(VueLazyload)
+
+Vue.use(VueLazyload, {
+  error: siteloading,
+  loading: siteloading,
+  attempt: 5,
+})
 
 new Vue({
   el: '#app',
