6ace110afa1d80adbc4999c16911ff662f557689
Nguyen Thien Dung <dungtoanhoc@yahoo.com.vn>
2020-01-22 13:38:20 +0200

    fix(build): Fix deprecated uglify js lib when build

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,15 +1,15 @@
 const webpack = require('webpack')
 const config = require('./webpack.base.conf')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const path = require('path')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 config.output.filename = 'vue-image-lightbox.min.js'
 config.output.libraryTarget = 'umd'
 config.output.library = 'Lightbox'
 
-config.entry =   path.resolve(__dirname, '../src/components/LightBox.vue')
+config.entry = path.resolve(__dirname, '../src/components/LightBox.vue')
 
 config.devtool = '#source-map'
 
@@ -21,27 +21,32 @@ config.module.rules.push({
   ],
 })
 
+config.optimization = {
+  minimize: true,
+  minimizer: [
+    new TerserPlugin({
+      sourceMap: true,
+      terserOptions: {
+        warnings: false,
+        compress: {
+          drop_debugger: true,
+          drop_console: true,
+          global_defs: {
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+          },
+        },
+
+        output: {
+          comments: false,
+        },
+      },
+    }),
+  ],
+}
+
 config.plugins = (config.plugins || []).concat([
   new MiniCssExtractPlugin({
     filename: 'vue-image-lightbox.min.css'
-  }),
-
-  new UglifyJSPlugin({
-    sourceMap: true,
-    uglifyOptions: {
-      warnings: false,
-      compress: {
-        drop_debugger: true,
-        drop_console: true,
-        global_defs: {
-          'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-        },
-      },
-
-      output: {
-        comments: false,
-      },
-    },
   }),
 
   new OptimizeCSSPlugin({
